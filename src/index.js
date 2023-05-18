15f6983d8dba663fce5d5b32a5f1b815455cc94d
Varun Kumar Chenna <varun.kumar@magicedtech.com>
2023-05-18 15:05:20 +0530

    json conversion working properly

--- src/index.js
+++ src/index.js
@@ -1,49 +1,86 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, dialog } = require("electron");
+const xlsx = require("xlsx");
+const fs = require("fs");
+const path = require("path");
+const { ipcMain } = require('electron');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
+// Listen for the file-upload event from the renderer process
+ipcMain.on('file-upload', () => {
+  handleFileUpload();
+});
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
     },
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-};
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+app.on("activate", function () {
+  if (mainWindow === null) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+// Function to handle file upload and conversion
+function handleFileUpload() {
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ["openFile"],
+      filters: [{ name: "XLSX Files", extensions: ["xlsx"] }],
+    })
+    .then((result) => {
+      if (!result.canceled) {
+        const filePath = result.filePaths[0];
+        const workbook = xlsx.readFile(filePath);
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = xlsx.utils.sheet_to_json(worksheet);
+        const jsonFilePath = filePath.replace(".xlsx", ".json");
+
+        fs.writeFile(jsonFilePath, JSON.stringify(jsonData, null, 2), (err) => {
+          if (err) {
+            dialog.showErrorBox(
+              "Error",
+              "An error occurred while converting the file."
+            );
+          } else {
+            dialog.showMessageBox(mainWindow, {
+              message: "File converted successfully!",
+              buttons: ["OK"],
+            });
+          }
+        });
+      }
+    });
+}
+
+// Expose handleFileUpload function to the renderer process
+// You can call this function from your HTML/JavaScript code
+global.handleFileUpload = handleFileUpload;
